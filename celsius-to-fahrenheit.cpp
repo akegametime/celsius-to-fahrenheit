829b005aa17ab57ac0ca6434e69d1fc4179072fa
akegametime <52166668+akegametime@users.noreply.github.com>
2019-07-04 20:54:31 +0700

    celsius-to-fahrenheit
    #include <iostream> 
    using namespace std;
    
    int main ()
    {
    	float Celsius; 
    	float Fahrenheit = 32.00;
    	cout<<"Enter Celsius temperature : ";
    	cin>>Celsius;
    	cout<<"Fahrenheit = "<<Celsius*1.80+Fahrenheit<<endl;
    	cout<<"Now weather in Thailand is"<<(Celsius*1.80+Fahrenheit>=70? " HOT " : " COOL ")<<endl;
    
    	
    	return(0);
    }

---
+++ celsius-to-fahrenheit.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std;
+
+int main ()
+{
+	float Celsius; 
+	float Fahrenheit = 32.00;
+	float temparature;
+	cout<<"Enter Celsius temperature : ";
+	cin>>Celsius;
+	cout<<"Fahrenheit = "<<Celsius*1.80+Fahrenheit<<endl;
+	cout<<"Now weather in Thailand is"<<(Celsius*1.80+Fahrenheit>=70? " HOT " : " COOL ")<<endl;
+
+	
+	return(0);
+}
+
+	
